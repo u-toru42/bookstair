3e037f80c85fbb11aae649add6d3ba8cbb806c27
u-toru42 <golt7040@gmail.com>
2022-12-13 10:57:03 +0900

    コントローラとビューを用意

---
+++ app/controllers/lines_controller.rb
@@ -0,0 +1,4 @@
+class LinesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  root to: 'bookshelfboards#index'
-  resources :bookshelfboards
+  get 'authors/index'
+  get 'tags/index'
+  get 'books/index'
+  get 'bookmarks/index'
+  get 'comments/index'
+  get 'reviews/index'
+  get 'checks/index'
+  get 'lines/index'
+  get 'bookshelf_line/index'
+  get 'bookshelf/index'
+  get 'bookshelf_follow/index'
+  root to: 'boards#index'
+  resources :boards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
