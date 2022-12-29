dd0f2a8cba144942bdd232b61532c871a7f820cc
u-toru42 <golt7040@gmail.com>
2022-12-29 16:26:29 +0900

    ログイン機能実装(ログアウト機能が未完成)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
