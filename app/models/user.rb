96d7d7229da01ee21e6ea2ec4bdd0a6ae20738bd
u-toru42 <golt7040@gmail.com>
2022-12-30 16:48:37 +0900

    i18n、decorator、ユーザー名絡む名をname->user_nameに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true, presence: true
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :user_name, presence: true, length: { maximum: 255 }
 end
