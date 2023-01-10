a07a866e4b830e6380b1388aa1dbd7d033970924
u-toru42 <golt7040@gmail.com>
2023-01-10 12:06:47 +0900

    Herokuデプロイ時の不具合を参考に追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Herokuデプロイ時にBlocked hostエラーが出る件を参考に追加
+  config.host.clear
 end
