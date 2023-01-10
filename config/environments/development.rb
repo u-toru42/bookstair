3a518435b07f2f55b7e1734aae164412117fa489
u-toru42 <golt7040@gmail.com>
2023-01-10 14:09:45 +0900

    Nodeのバージョン/development.rbを修正
    Nodeのバージョンを16系で統合、config.host.clearは不具合が出るので削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Herokuデプロイ時にBlocked hostエラーが出る件を参考に追加
-  config.host.clear
+  # config.host.clear
 end
