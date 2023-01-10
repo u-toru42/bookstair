c1784af6552b546df56ac52dc382546c8cf76ce0
u-toru42 <golt7040@gmail.com>
2023-01-10 09:20:48 +0900

    Docker関係修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
 bin/rails db:migrate
-bundle exec rake ridgepole:apply
-bin/dev
-# bin/rails db:seed
+bin/rails db:seed
 
 rm -f tmp/pids/server.pid && bin/rails s
