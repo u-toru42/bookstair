9686314fc1942f044dad2e5e8f1e19c2f833f44a
u-toru42 <golt7040@gmail.com>
2023-01-11 14:51:37 +0900

    フッター/ビューの作成開始

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def rakuten_book_thumbnail(book)
-    book['mediumImageUrl'] == "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/noimage_01.gif?_ex=120x120" ? 'no_image.png' : book['mediumImageUrl']
+    book['largeImageUrl'] == "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/noimage_01.gif?_ex=120x120" ? 'no_image.png' : book['largeImageUrl']
   end
 
   def set_rakuten_book_params(book)
     #volumeInfoの中が必要な項目のみになるようsliceを使って絞りこむ
-    book['item'].slice('title', 'author', 'salesDate', 'mediumImageUrl', 'itemUrl', 'isbn')
+    book['item'].slice('title', 'author', 'salesDate', 'largeImageUrl', 'itemUrl', 'isbn')
   end
 end
