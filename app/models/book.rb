750a4d8dbdda2e6ddbb9095016d561d35d9e7908
u-toru42 <golt7040@gmail.com>
2023-01-21 18:06:25 +0900

    bookテーブルのPKをisbnに変更

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,4 @@
 class Book < ApplicationRecord
-  def save_with_author(author)
-    ActiveRecord::Base.transaction do
-      self.save!
-      self.author = author.uniq.reject(&:blank?).map { |name| Author.find_or_initialize_by(name: name.strip) } unless author.nil?
-    end
-    true
-    rescue StandardError
-      false
-  end
+  self.primary_key = :isbn
+  has_many :reviews, primary_key: "isbn", foreign_key: "isbn"
 end
