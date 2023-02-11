9c9e66129afd0a4fa81a71255183f8963513d3e4
u-toru42 <golt7040@gmail.com>
2023-02-11 19:40:11 +0900

    Revert "deviseの適用"
    This reverts commit ca135bb425a02daeb0a818e11dcb7098cb37ab76.

--- app/javascript/flash.js
+++
@@ -1,6 +0,0 @@
-// notification を×で閉じれるように
-for (const element of document.querySelectorAll('.notification > .delete')) {
-    element.addEventListener('click', e => {
-        e.target.parentNode.classList.add('is-hidden');
-    });
-}
