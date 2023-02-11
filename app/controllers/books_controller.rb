aa343d7d137456caefa787bf83bbcdc16acf518e
u-toru42 <golt7040@gmail.com>
2023-02-11 13:55:33 +0900

    isbnのルート、viewの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
+<<<<<<< Updated upstream
   
+=======
+
+>>>>>>> Stashed changes
   def new
     @book = Book.new
   end
@@ -16,7 +20,11 @@ class BooksController < ApplicationController
   def destroy
     # button_toのvalueがidになっているがisbnに変換することは可能か
     book = Book.find_by(isbn: params[:isbn])
+<<<<<<< Updated upstream
     book.destroy
+=======
+    book.delete
+>>>>>>> Stashed changes
     redirect_to books_path, danger: "「#{book[:title]}」を削除しました。", status: :see_other
   end
   
@@ -37,6 +45,20 @@ class BooksController < ApplicationController
     elsif params[:author_search] && params[:title_search]
       @books = RakutenWebService::Books::Book.search(title: params[:title_search], author: params[:author_search])
     end
+<<<<<<< Updated upstream
+=======
+  end
+
+  def show
+    @book = Book.find_by!(isbn: params[:isbn])
+    @comment = Comment.new
+    @comments = @book.comments.includes(:user).order(created_at: :desc)
+    # @book = Book.includes(:user).find_by!(isbn: params[:isbn])
+    # @comments = @book.comments.all
+    # @comment = @book.comments.build(user_id: current_user.id) if current_user
+    # @comment = Comment.new(isbn: params[:isbn])
+    # binding.irb
+>>>>>>> Stashed changes
   end
 
   private  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
-  authenticates_with_sorcery!
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+    def self.guest
+      find_or_create_by!(email: "guest@example.com") do |user|
+        user.password = SecureRandom.urlsafe_base64
+    end
+  end
 
+<<<<<<< Updated upstream
   validates :email, uniqueness: true, presence: true
   validates :user_name, presence: true, length: { maximum: 255 }
 
   has_many :books, through: :bookshelves
   has_many :bookshelves, dependent: :destroy
+=======
+  has_many :books, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def my_comment?(comment)
+    self == comment.user
+  end
+
+  def own?(object)
+    id == object.user_id
+  end
+>>>>>>> Stashed changes
 end
