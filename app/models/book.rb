98ca0cedfaca440f335130474be68f35823fa8ff
u-toru42 <golt7040@gmail.com>
2023-02-11 18:26:14 +0900

    stash変更分をマージ

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,15 +12,6 @@
 #  updated_at      :datetime         not null
 #
 class Book < ApplicationRecord
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  self.primary_key = :isbn
-  has_many :users, primary_key: "isbn", foreign_key: "isbn", through: :bookshelves
-  validates :isbn, :uniqueness => {:scope => :isbn}
-end
-=======
-=======
->>>>>>> Stashed changes
   self.primary_key = 'isbn'
   validates :isbn, uniqueness: { scope: :isbn }
   def to_param
@@ -28,7 +19,3 @@ end
   end
   has_many :comments, dependent: :destroy
 end
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,4 @@
 ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 end
