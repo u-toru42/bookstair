c010f53e77e598527cc6998cef457a90eaf38067
u-toru42 <golt7040@gmail.com>
2023-02-11 14:44:48 +0900

    stashの変更を適用

--- app/models/book.rb
+++ app/models/book.rb
@@ -13,11 +13,14 @@
 #
 class Book < ApplicationRecord
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   self.primary_key = :isbn
   has_many :users, primary_key: "isbn", foreign_key: "isbn", through: :bookshelves
   validates :isbn, :uniqueness => {:scope => :isbn}
 end
 =======
+=======
+>>>>>>> Stashed changes
   self.primary_key = 'isbn'
   validates :isbn, uniqueness: { scope: :isbn }
   def to_param
@@ -25,4 +28,7 @@ end
   end
   has_many :comments, dependent: :destroy
 end
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
