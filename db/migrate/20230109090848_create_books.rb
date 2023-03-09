1916aabf05aa88e687ca9f1afece1eabbbe29434
u-toru42 <golt7040@gmail.com>
2023-03-09 18:12:26 +0900

    ridgepoleを削除しMigrationファイルを作成

---
+++ db/migrate/20230109090848_create_books.rb
@@ -0,0 +1,17 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table "books", id: false do |t|
+      t.bigint :isbn, primary_key: true
+      t.string :title
+      t.string :author
+      t.text :sales_date
+      t.text :large_image_url
+      t.text :item_url
+      t.text :item_caption
+      t.string :item_price
+
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+  end
+end
