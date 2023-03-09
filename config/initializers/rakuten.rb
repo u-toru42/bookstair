a88ad6badea36c04bba67b0737eaca921ddc2755
u-toru42 <golt7040@gmail.com>
2023-03-09 15:51:47 +0900

    idをstringに変換

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,8 +1,8 @@
 RakutenWebService.configure do |c|
   # (必須) アプリケーションID
-  c.application_id = Rails.application.credentials.rakuten[:application_id]
+  c.application_id = Rails.application.credentials.rakuten[:application_id].to_s
 
   # (任意) 楽天アフィリエイトID
-  c.affiliate_id = Rails.application.credentials.rakuten[:application_id]
+  # c.affiliate_id = Rails.application.credentials.rakuten[:affiliate_id]
 
 end
