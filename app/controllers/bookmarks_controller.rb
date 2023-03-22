22963ab529ff7679a92988a7d8acabfea1fc614a
u-toru42 <golt7040@gmail.com>
2023-03-22 16:50:30 +0900

    Fix: フラッシュメッセージのメソッドを修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,7 +3,7 @@ class BookmarksController < ApplicationController
   def create
     bookmark = current_user.bookmarks.new(bookmark_params)
     if bookmark.save_with_tags(tag_names: params.dig(:bookmark, :tag_names).split(',').uniq)
-      redirect_to bookmark_path(bookmark), success: 'しおりを作成しました'
+      redirect_to bookmark_path(bookmark), notice: 'しおりを作成しました'
     else
       flash.now[:danger] = 'しおり作成失敗'
       @book = Book.find_by!(isbn: params[:isbn])
@@ -17,7 +17,7 @@ class BookmarksController < ApplicationController
   def update
     if @bookmark.update(bookmark_params)
       @bookmark.save_with_tags(tag_names: params.dig(:bookmark, :tag_names).split(',').uniq)
-      redirect_to @bookmark, success: 'しおりを作成しました'
+      redirect_to @bookmark, notice: 'しおりを作成しました'
     else
       render :edit
     end
@@ -26,7 +26,7 @@ class BookmarksController < ApplicationController
   def destroy
     bookmark = Bookmark.find(params[:id])
     bookmark.destroy
-    redirect_to bookmark.book, success: 'しおりが削除されました'
+    redirect_to bookmark.book, danger: 'しおりが削除されました'
   end
 
   private
