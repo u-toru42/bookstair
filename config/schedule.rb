eb72dfe2de24377de3bc29cd90a1e6067365622f
u-toru42 <golt7040@gmail.com>
2023-03-29 17:48:46 +0900

    Fix: schedule.rbの権限譲渡を追加する

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,15 @@
 # config/schedule.rb
+# Rails.rootを使用するようにする（※wheneverはrailsの起動を前提とするため）
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+
+# コンテナ起動時の環境変数をcron独自の環境変数にパス
+ENV.each { |k, v| env(k, v) }
+
+#出力先ログ
+set :output, 'log/cron_monthly_batch.log'
+#実行環境
+set :environment, :development
+
 # 毎日5時01分と17時01分にジョブを実行する
 every '1 5,17 * * *' do
   runner "FetchFeedsJob.perform_now", ENV['FEED_URLS'] || ''
