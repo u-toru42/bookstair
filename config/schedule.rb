8931fdf6b26d19302c4346343f46338a79372b8b
u-toru42 <golt7040@gmail.com>
2023-04-04 09:36:59 +0900

    Fix: cronの実行環境を修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,24 @@
-# config/schedule.rb
-# Rails.rootを使用するようにする（※wheneverはrailsの起動を前提とするため）
+# Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + "/environment")
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
 
-# コンテナ起動時の環境変数をcron独自の環境変数にパス
-ENV.each { |k, v| env(k, v) }
-
-#出力先ログ
-set :output, 'log/cron_monthly_batch.log'
-#実行環境
-set :environment, :development
+# stagingのみで実行
+if rails_env.to_sym != :development
+  # clear cache
+  every 10.minutes do
+    begin
+      rake 'sample_task:all', :environment_variable => "RAILS_ENV", :environment => "development"
+    rescue => e
+      Rails.logger.error("aborted rake task")
+      raise e
+    end
+  end
+end
 
 # 毎日5時01分と17時01分にジョブを実行する
 every '1 5,17 * * *' do
