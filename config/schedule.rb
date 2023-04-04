d2f4e05dddcecaf9fe326005780a5a415e6b5ae4
u-toru42 <golt7040@gmail.com>
2023-04-04 18:49:03 +0900

    Fix: cronの修正、15分ごとのcron実行テスト

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,31 +1,16 @@
-# Rails.rootを使用するために必要
-require File.expand_path(File.dirname(__FILE__) + "/environment")
-# cronを実行する環境変数
+require_relative "config/environment"
+
 rails_env = ENV['RAILS_ENV'] || :development
-# cronを実行する環境変数をセット
+
 set :environment, rails_env
-# cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
 
-# stagingのみで実行
-if rails_env.to_sym != :development
-  # clear cache
-  every 10.minutes do
-    begin
-      rake 'sample_task:all', :environment_variable => "RAILS_ENV", :environment => "development"
-    rescue => e
-      Rails.logger.error("aborted rake task")
-      raise e
-    end
-  end
-end
-
-# 毎日5時01分と17時01分にジョブを実行する
-every '1 5,17 * * *' do
+every 15.minutes do
   runner "FetchFeedsJob.perform_now", ENV['FEED_URLS'] || ''
 end
 
-# 毎週月曜日にジョブを実行する
-every '0 0 * * 1' do
-  runner "UpdateFeedsJob.perform_now"
+every :hour do
+  every :hour, at: '00:00' do
+    runner "UpdateFeedsJob.perform_now"
+  end
 end
