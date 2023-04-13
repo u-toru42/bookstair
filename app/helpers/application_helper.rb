05c978e46b9b25302017f11cbab3d2d8d227ae37
u-toru42 <golt7040@gmail.com>
2023-04-13 18:17:17 +0900

    Fix: メタタグセット、背景画像修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,32 @@ module ApplicationHelper
     tags = JSON.parse(response.body)
     tags.map { |tag| tag['name'] }
   end
+
+  def default_meta_tags
+    {
+      site: 'BOOKSTAIR',
+      title: 'IT・コンピュータの書籍好きに',
+      reverse: true,
+      separator: '|',
+      description: 'BOOKSTAIRはIT・コンピュータの書籍の専門サービスです。',
+      keywords: 'IT, コンピュータ, プログラミング',
+      charset: 'UTF-8',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: image_url('favicon.ico'),
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('og_image.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('og_image.png')
+      }
+    }
+  end
 end
