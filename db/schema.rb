f09772638ae7bab6d908ccd8887cb20f008af9e1
u-toru42 <golt7040@gmail.com>
2023-04-18 12:52:35 +0900

    Fix: Likeテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_015732) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_045039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,11 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_015732) do
     t.string "title"
     t.text "content"
     t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
