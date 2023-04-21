4d70c74d8d36c41737fdfb291a098651b0146d50
u-toru42 <golt7040@gmail.com>
2023-04-21 20:56:08 +0900

    Add: ゲストログインのroutes.rbを復活させ、redis_urlをcredentialsで実装

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,26 +1,26 @@
 require 'sidekiq/api'
 require 'sidekiq-cron'
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.on(:startup) do
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(File.expand_path('../../../config/schedule.yml', __FILE__))
-    end
-  end
-end
-
-if Rails.env.development?
-  Sidekiq.configure_server do |config|
-    config.on(:startup) do
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(File.expand_path('../../../config/schedule.yml', __FILE__))
-    end
-  end
-end
-
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://127.0.0.1:6379' }
+# if Rails.env.production?
+#   Sidekiq.configure_server do |config|
+#     config.on(:startup) do
+#       Sidekiq::Cron::Job.load_from_hash YAML.load_file(File.expand_path('../../../config/schedule.yml', __FILE__))
+#     end
+#   end
 # end
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://127.0.0.1:6379' }
+# if Rails.env.development?
+#   Sidekiq.configure_server do |config|
+#     config.on(:startup) do
+#       Sidekiq::Cron::Job.load_from_hash YAML.load_file(File.expand_path('../../../config/schedule.yml', __FILE__))
+#     end
+#   end
 # end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: Rails.application.credentials.redis[:url].to_s }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: Rails.application.credentials.redis[:url].to_s }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,10 @@ Rails.application.routes.draw do
   #   mount Sidekiq::Web => '/sidekiq'
   # end
   
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+  
   resources :books, param: :isbn, constraints: { code: /\d+/ } do
     collection do 
       get :search
