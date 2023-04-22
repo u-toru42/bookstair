0330bc3680f4b6e0aee66f5b332c6dadb199e073
u-toru42 <golt7040@gmail.com>
2023-04-22 09:45:53 +0900

    Fix: schedule.ymlの読み込みを追加

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,26 +1,14 @@
-require 'sidekiq/api'
-require 'sidekiq-cron'
-
-# if Rails.env.production?
-#   Sidekiq.configure_server do |config|
-#     config.on(:startup) do
-#       Sidekiq::Cron::Job.load_from_hash YAML.load_file(File.expand_path('../../../config/schedule.yml', __FILE__))
-#     end
-#   end
-# end
-
-# if Rails.env.development?
-#   Sidekiq.configure_server do |config|
-#     config.on(:startup) do
-#       Sidekiq::Cron::Job.load_from_hash YAML.load_file(File.expand_path('../../../config/schedule.yml', __FILE__))
-#     end
-#   end
-# end
-
 Sidekiq.configure_server do |config|
   config.redis = { url: Rails.application.credentials.redis[:url].to_s }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: Rails.application.credentials.redis[:url].to_s }
+end
+
+schedule_file = "config/schedule.yml"
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Scheduler.enabled = true
+  Sidekiq::Scheduler.load_schedule! schedule_file
 end
