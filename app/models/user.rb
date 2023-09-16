bd9e5f7e06064abdd4a51e2fec35d9c56de89327
u-toru42 <golt7040@gmail.com>
2023-09-16 16:38:04 +0900

    Add: メール認証用のDB修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,16 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  name                   :string           default(""), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
